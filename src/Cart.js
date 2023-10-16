22a8dc18abbc8cd7201e5aaab2c41752003f4d7a
sanooj.m <sanooj.m@zerone-consulting.com>
2023-10-16 15:59:42 +0530

    image added to show

--- src/Cart.js
+++ src/Cart.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addItem, deleteItem } from "./actions/cartAction";
+import logo from "./logo.svg";
 
 const Cart = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
-    <div className="cart" style={{ backgroundColor: 'red' }}>
+    <div className="cart" style={{ backgroundColor: "red" }}>
       <h2>Number of items in Cart: {state.numOfItems}</h2>
       <button
         onClick={() => {
           dispatch(addItem());
         }}
       >
-        <img src="https://img.icons8.com/material-outlined/24/000000/add-shopping-cart--v1.png" />
+        <img src={logo} />
         Add Item to Cart
       </button>
       <button
