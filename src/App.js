a50270105eda5f51ffd0a817e0a8393a79360d1c
sanooj.m <sanooj.m@zerone-consulting.com>
2023-10-16 13:20:04 +0530

    change in route

--- src/App.js
+++ src/App.js
@@ -2,29 +2,12 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
 import Cart from "./Cart";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename="/cra-webcomponents">
-        <Routes>
-          <Route exact path="/" element={<h1><a href="/page1">Home Page</a></h1>} />
-          <Route
-            exact
-            path="page1"
-            element={
-              <Provider store={store}>
-                <Cart />
-              </Provider>
-            }
-          />
-          <Route exact path="page2" element={<h1>Home Page</h1>} />
-          <Route exact path="page3" element={<h1>Home Page</h1>} />
-        </Routes>
-      </BrowserRouter>
-      <Provider store={store}>{/* <Cart /> */}</Provider>
-    </>
+    <Provider store={store}>
+      <Cart />
+    </Provider>
   );
 }
 
